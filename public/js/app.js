e22c5806db00ae711e611d1b1111011da802b22d
aliabirshuvro <aliabirshuvro@gmail.com>
2020-05-04 11:47:09 +0600

    remove unnecessary console.lg

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,8 @@
-console.log('client side is loaded')
 
 const weatherForm = document.querySelector('form')
 const search= document.querySelector('input')
 const messageOne= document.querySelector('#message-1')
 const messageTwo= document.querySelector('#message-2')
-
-
 
 weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault()
