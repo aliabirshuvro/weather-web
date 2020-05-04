ecd6c971dded2471b15b5a96aff1110ecb12fbc1
aliabirshuvro <aliabirshuvro@gmail.com>
2020-05-04 11:43:21 +0600

    initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.openweathermap.org/data/2.5/onecall?lat='+latitude+'&lon='+longitude+'&exclude=hourly,daily&appid=fe224b61c85ac08fb3774685d7b7fe85'
+    //const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            const t=(body.current.temp-273).toFixed(2)
+            callback(undefined, body.current.weather[0].description + '! It is currently ' + t + ' degress out. Humidity is ' + body.current.humidity + '%.')
+        }
+    })
+}
+
+module.exports = forecast
